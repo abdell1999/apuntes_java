ab2149ec59b541901dda13a441b50069b9823ca8
abdell (Linux) <abdellahbukadduh99@gmail.com>
2020-10-26 18:37:26 +0100

    Ejercicio11 manera facil
    Ejercicio11 de 'Problemas Java' resuelto sin Strings, con divisiones entre 10

--- Problemas Java/Ejercicio11.java
+++ Problemas Java/Ejercicio11.java
@@ -7,6 +7,8 @@ public class Ejercicio11{
 		
 		Scanner entrada = new Scanner(System.in);
 		int numero;
+		int contador = 0;
+/*
 		String numeroS;
 		
 		do{
@@ -21,6 +23,37 @@ public class Ejercicio11{
 		
 		numeroS= Integer.toString(numero); //Tambien se puede usar String.valueOf();
 		System.out.println("El numero introducido tiene "+numeroS.length()+" cifras");
+*/
+
+                
+                do{
+                System.out.println("Introduce un numero entre 0 y 9999");
+                numero = entrada.nextInt();
+                
+                
+                
+                }while(!(numero>=0) || !(numero<=9999));
+
+
+		if (numero ==0){
+
+			contador = 1;
+		}
+ 
+		while (numero!=0){
+		numero /= 10;
+		contador ++;
+
+		}
+
+
+		System.out.println("El numero tiene "+contador+" cifras");
+
+
+
+
+
+
 		
 		}
 	}
